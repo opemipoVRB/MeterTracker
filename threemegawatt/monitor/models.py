acd13de096a639ed2ccf21d8cea4605da9660ef0
opemipoVRB <opemipodurodola@gmail.com>
2019-07-30 13:22:49 +0100

    Created models and Custom User model

--- threemegawatt/monitor/models.py
+++ threemegawatt/monitor/models.py
@@ -1,3 +1,123 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+from django.contrib.auth.models import PermissionsMixin
+
+from django.contrib.auth.models import BaseUserManager
+
+
+# Create manager here
+
+
+class UserProfileManager(BaseUserManager):
+    """
+
+    Helps django work with custom user models
+
+    """
+
+    def create_user(self, email, first_name, last_name, password):
+        """
+        Creates new user profile object
+        :param last_name:
+        :param first_name:
+        :param email:
+        :param password:
+        :return:
+        """
+
+        if not email:
+            raise ValueError('Provide an email address for user.')
+
+        email = self.normalize_email()
+
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        """
+        Creates and saves a new superuser with given details
+        :param last_name:
+        :param first_name:
+        :param email:
+        :param password:
+        :return:
+        """
+
+        user = self.create_user(email, first_name, last_name)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self.db)
+
+        return user
+
 
 # Create your models here.
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+
+    Representing the user profile
+
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """
+        Used to get user's  full name
+        :return:
+
+        """
+
+        return self.first_name + self.last_name
+
+    def get_short_name(self):
+        """
+        Used to get user's  short name
+        :return:
+
+        """
+
+        return self.first_name
+
+    def __str__(self):
+        """
+        Used to convert object to a string
+
+        :return:
+        """
+
+        return self.email
+
+
+class Plant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, blank=False)
+
+
+class Datapoint(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    plant_id = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    energy_expected = models.DecimalField()
+    energy_observed = models.DecimalField()
+    irradiation_expected = models.DecimalField()
+    irradiation_observed = models.DecimalField()
+
--- threemegawatt/threemegawatt/settings.py
+++ threemegawatt/threemegawatt/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'monitor',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Set User Model
+AUTH_USER_MODEL = 'monitor.UserProfile'
+
